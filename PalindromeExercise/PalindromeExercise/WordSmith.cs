b32ae8adbfeb9893923926b0231c07342eb4236f
Dallas Yu <dallas_yu@hotmail.com>
2025-07-11 20:30:14 -0500

    done

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+
+namespace PalindromeExercise;
+
+public class WordSmith
+{
+    public bool IsAPalindrome(string str)
+    {
+        str = str.ToLower();
+        string reversed = new string(str.Reverse().ToArray());
+        return str == reversed;
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,13 +1,33 @@
 using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)] 
+        public void Test1(string str, bool expected)
         {
+            var word = new WordSmith();
+            
+            var result = word.IsAPalindrome(str);
+
+            Assert.Equal(expected, result);
+
+
+        }
+
+        [Theory]
+        [InlineData("hello", false)]
+        public void Test2(string str, bool expected)
+        {
+            var word = new WordSmith();
+
+            var result = word.IsAPalindrome(str);
+
+            Assert.Equal(expected, result);
 
         }
     }
