f217d0e738fcb60d363ede074de498a88d6e5a32
Dallas Yu <dallas_yu@hotmail.com>
2025-07-16 10:46:10 -0500

    added more inline data

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -7,27 +7,18 @@ namespace PalindromeExerciseTests
     public class UnitTest1
     {
         [Theory]
-        [InlineData("racecar", true)] 
+        [InlineData("racecar", true)]
+        [InlineData("hello", false)]
+        [InlineData( "bob", true)]
+        [InlineData("gag", true)]
         public void Test1(string str, bool expected)
         {
             var word = new WordSmith();
-            
-            var result = word.IsAPalindrome(str);
-
-            Assert.Equal(expected, result);
-
-
-        }
-
-        [Theory]
-        [InlineData("hello", false)]
-        public void Test2(string str, bool expected)
-        {
-            var word = new WordSmith();
 
             var result = word.IsAPalindrome(str);
 
             Assert.Equal(expected, result);
+
 
         }
     }
